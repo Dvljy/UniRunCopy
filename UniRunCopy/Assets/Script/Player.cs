b2e689e0f5611e7afef691c14bc658cca4df55d9
재윤 <gojun1352@gmail.com>
2022-07-13 12:36:30 +0900

    UniRunCopy
    220713

--- UniRunCopy/Assets/Script/Player.cs
+++ UniRunCopy/Assets/Script/Player.cs
@@ -7,36 +7,77 @@ public class Player : MonoBehaviour
     [SerializeField] float JumpPower;
 
     Rigidbody2D rigid;
-    Animation anim;
+    Animator anim;
+    AudioSource playeraudio;
+    AudioClip Jumpaudio;
+    Renderer PyColor;
+
+    int JumpCount;
 
     bool isJump = false;
+    bool isGrounded = true;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animation>();
-        
+        anim = GetComponent<Animator>();
+        playeraudio = GetComponent<AudioSource>();
     }
+
+
 
     void Update()
     {
         PJ();
-        
+        //Die();
+
+
     }
 
     void PJ()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && JumpCount < 1)
         {
-            isJump = true;
-            rigid.AddForce(Vector3.up * JumpPower, ForceMode2D.Impulse);
-              
+            JumpCount++;
+
+            rigid.velocity = Vector3.up * JumpPower;
+            playeraudio.Play();
+            
+
         }
-        
+        if (isGrounded)
+        {
+            JumpCount = 0;
+            anim.SetBool("isJump", false);
+            anim.SetBool("isGrounded", true);
+        }
+        else
+        {
+            anim.SetBool("isJump", true);
+            anim.SetBool("isGrounded", false);
+        }
+
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    /*void Die()
     {
+        if (true)
+        {
+            PyColor.material.color = Color.red;
+
+            Destroy(gameObject, 3f);
+        }
         
+    }*/
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
     }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+    }
+
 }
