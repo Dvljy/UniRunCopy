053833b3af93da54164541d3f81e760877b97e7b
재윤 <gojun1352@gmail.com>
2022-08-09 10:50:42 +0900

    220809
    스테이지2 UI구현

--- UniRunCopy/Assets/Script/Stage 2/PlayerCR.cs
+++ UniRunCopy/Assets/Script/Stage 2/PlayerCR.cs
@@ -5,20 +5,65 @@ using UnityEngine;
 public class PlayerCR : MonoBehaviour
 {
     Rigidbody2D rigid;
+    Animator anim;
+
     [SerializeField] float speed;
+    float JumpCount = 2;
+    bool isGrounded = true;
+    
 
 
     void Start()
     {
+        anim = GetComponent<Animator>();
         rigid = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Jump();
 
     }
 
-    
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            JumpCount++;
+            rigid.velocity = new Vector3(0,speed,0);
+            
+        }
+        else
+        {
+            
+            
+        }
+        if (!isGrounded)
+        {
+            anim.SetBool("isJump", true);
+        }
+        else if (isGrounded)
+        {
+            anim.SetBool("isJump", false);
+        }
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.contacts[0].normal.y > 0.7f)
+        {
+            isGrounded = true;
+            JumpCount = 0;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (transform.position.y > 0.1f)
+        {
+            isGrounded = false;
+        }
+    }
 }
